9e44c7255f818247ea473cb533c3a1d7d9163f0a
Kevin Kelly <kvinklly@gmail.com>
2017-11-17 13:02:23 -0600

    added percent, plus-minus, square-root and clear buttons, also rounding of decimals

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Calculator app using CSS grid for layout and jQuery and JavaScript for the logic.
 
 Next steps to complete:
-- Set up variable to hold displayed value and last value
-- Set up number buttons to add to the displayed value
--- Leading zeros
--- Decimal point
--- Displaying large numbers
-- Set up operators 
-- Add support for keypresses on the keyboard
+* Set up variable to hold displayed value and last value
+* Set up number buttons to add to the displayed value
+  - Leading zeros
+  - Decimal point
+  - Displaying large numbers
+* Set up operators
+* Add support for keypresses on the keyboard
--- js/calc.js
+++ js/calc.js
@@ -1,33 +1,72 @@
 var display = '0';
 var actionStack = [display.value];
+var displayNumberExtendable = true;
+var currentError = '';
 
 var setAllClear = function () {
   display = '0';
   actionStack = [];
-  displayValue(display);
+  displayNumberExtendable = true;
+  // displayValue(display);
 };
 
 var displayValue = function (str) {
+  if (!str) {
+    str = display;
+  }
+  if (str.length > 10) {
+    var decimalIndex = str.indexOf('.');
+    if (str.indexOf('.') > -1) {
+      str = (Number(str).toPrecision(9)).toString();
+    }
+  }
+  if (display === 'NaN') {
+    displayNumberExtendable = false;
+  }
   $('#display').text(str);
 };
 
+var displayError = function (str) {
+  if (!str) {
+    str = currentError;
+  }
+  $('#error').text(currentError);
+}
+
 setAllClear();
+displayValue();
 
 $(".button").on("click", function() {
   if(this.id === 'all-clear') {
     setAllClear();
   } else if (Number(this.id) >= 0) {
-    if (Number(display) === 0) {
+    if ((Number(display) === 0 && display.indexOf('.') === -1) || !displayNumberExtendable) {
       display = this.id;
+      displayNumberExtendable = true;
     } else {
       display += this.id;
     }
-    displayValue(display);
   } else if (this.id === 'decimal') {
     if (display.indexOf('.') === -1) {
       display += '.';
     }
-    displayValue(display);
+  } else if (this.id === 'percent') {
+    display = (Number(display) / 100).toString();
+    displayNumberExtendable = false;
+  } else if (this.id === 'plus-minus') {
+    display = (- Number(display)).toString();
+  } else if (this.id === 'square-root') {
+    if (Number(display) < 0) {
+      currentError = 'Square root of a negative number is not a real number.';
+    } else {
+      display = (Math.sqrt(Number(display))).toString();
+      displayNumberExtendable = false;
+    }
+  } else if (this.id === 'clear') {
+    display = '0';
   }
+  displayError(currentError);
+  currentError = '';
+  displayValue(display);
   console.log(this.id);
 });
