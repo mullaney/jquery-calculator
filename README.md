c310b1cb11ee3da303e2ae5753785b844a0ab8be
Kevin Kelly <kvinklly@gmail.com>
2017-11-17 09:48:10 -0600

    added number buttons, decimal point and all clear

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Calculator app using CSS grid for layout and jQuery and JavaScript for the logic.
 
 Next steps to complete:
-- Create basic layout of calculator using css grid with a mobile first design
-- Hook up all the buttons with unique ids
-- Add click effect when buttons are pressed
+- Set up variable to hold displayed value and last value
+- Set up number buttons to add to the displayed value
+-- Leading zeros
+-- Decimal point
+-- Displaying large numbers
+- Set up operators 
 - Add support for keypresses on the keyboard
--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,33 @@
-$("#hero").html("Calculator - jQuery Ready");
+var display = '0';
+var actionStack = [display.value];
+
+var setAllClear = function () {
+  display = '0';
+  actionStack = [];
+  displayValue(display);
+};
+
+var displayValue = function (str) {
+  $('#display').text(str);
+};
+
+setAllClear();
 
 $(".button").on("click", function() {
-  console.log(this);
+  if(this.id === 'all-clear') {
+    setAllClear();
+  } else if (Number(this.id) >= 0) {
+    if (Number(display) === 0) {
+      display = this.id;
+    } else {
+      display += this.id;
+    }
+    displayValue(display);
+  } else if (this.id === 'decimal') {
+    if (display.indexOf('.') === -1) {
+      display += '.';
+    }
+    displayValue(display);
+  }
+  console.log(this.id);
 });
