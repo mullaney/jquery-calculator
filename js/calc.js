34333f6d93a1c939212b49896243085cc7241b0a
Kevin Kelly <kvinklly@gmail.com>
2017-11-17 21:48:19 -0600

    behavior for hitting equal over and over

--- js/calc.js
+++ js/calc.js
@@ -2,12 +2,19 @@ var display = '0';
 var actionStack = [display.value];
 var displayNumberExtendable = true;
 var memory = 0;
+var lastNumber = 0;
+var lastOperator = '';
+var justEqualed = false;
+var arg1 = 0;
+var arg2 = 0;
 
 var setAllClear = function () {
   display = '0';
   actionStack = [];
   var memory = 0;
   displayNumberExtendable = true;
+  lastNumber = 0;
+  lastOperator = '';
 };
 
 var displayValue = function (str) {
@@ -16,12 +23,10 @@ var displayValue = function (str) {
   }
   if (str.length > 10) {
     var decimalIndex = str.indexOf('.');
+    console.log(decimalIndex);
     if (str.indexOf('.') > -1) {
-      str = (Number(str).toPrecision(9)).toString();
+      str = (Number(str).toPrecision(9 - decimalIndex)).toString();
     }
-  }
-  if (display === 'NaN') {
-    displayNumberExtendable = false;
   }
   $('#display').text(str);
 };
@@ -72,6 +77,39 @@ $(".button").on("click", function() {
       memory += Number(display);
       displayNumberExtendable = false;
     }
+  } else if (['divide', 'multiply', 'subtract', 'add'].indexOf(this.id) >= 0) {
+    lastNumber = Number(display);
+    lastOperator = this.id;
+    displayNumberExtendable = false;
+  } else if (this.id === 'equal') {
+    if (lastOperator && display != 'Error') {
+      if (lastOperator === 'divide' && Number(display) === 0) {
+          display = 'Error';
+      } else {
+        if (justEqualed == false) {
+          arg1 = lastNumber;
+          arg2 = Number(display);
+        } else {
+          // arg1 = Number(display);
+          // arg2 = lastNumber;
+        }
+        if (lastOperator === 'divide') {
+          display = (arg1 / arg2).toString();
+        } else if (lastOperator === 'multiply') {
+          display = (arg1 * arg2).toString();
+        } else if (lastOperator === 'subtract') {
+          display = (arg1 - arg2).toString();
+        } else if (lastOperator === 'add') {
+          display = (arg1 + arg2).toString();
+        }
+        arg1 = Number(display);
+        displayNumberExtendable = false;
+        justEqualed = true;
+      }
+    }
+  }
+  if (justEqualed == true && this.id !== 'equal') {
+    justEqualed = false;
   }
   displayValue(display);
   console.log(this.id);
